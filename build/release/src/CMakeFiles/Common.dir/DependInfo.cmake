0af2e5f8a3bdc4456b695443f54bd92d3f9bba5a
LiteChain <38558930+LiteChain@users.noreply.github.com>
2018-04-23 15:18:54 +0200

    Add files via upload

---
+++ build/release/src/CMakeFiles/Common.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/coins/DirhamCli/src/Common/Base58.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Common.dir/Common/Base58.cpp.o"
+  "/root/coins/DirhamCli/src/Common/BlockingQueue.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Common.dir/Common/BlockingQueue.cpp.o"
+  "/root/coins/DirhamCli/src/Common/CommandLine.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Common.dir/Common/CommandLine.cpp.o"
+  "/root/coins/DirhamCli/src/Common/ConsoleHandler.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Common.dir/Common/ConsoleHandler.cpp.o"
+  "/root/coins/DirhamCli/src/Common/ConsoleTools.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Common.dir/Common/ConsoleTools.cpp.o"
+  "/root/coins/DirhamCli/src/Common/IInputStream.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Common.dir/Common/IInputStream.cpp.o"
+  "/root/coins/DirhamCli/src/Common/IOutputStream.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Common.dir/Common/IOutputStream.cpp.o"
+  "/root/coins/DirhamCli/src/Common/JsonValue.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Common.dir/Common/JsonValue.cpp.o"
+  "/root/coins/DirhamCli/src/Common/Math.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Common.dir/Common/Math.cpp.o"
+  "/root/coins/DirhamCli/src/Common/MemoryInputStream.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Common.dir/Common/MemoryInputStream.cpp.o"
+  "/root/coins/DirhamCli/src/Common/PathTools.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Common.dir/Common/PathTools.cpp.o"
+  "/root/coins/DirhamCli/src/Common/ScopeExit.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Common.dir/Common/ScopeExit.cpp.o"
+  "/root/coins/DirhamCli/src/Common/SignalHandler.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Common.dir/Common/SignalHandler.cpp.o"
+  "/root/coins/DirhamCli/src/Common/StdInputStream.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Common.dir/Common/StdInputStream.cpp.o"
+  "/root/coins/DirhamCli/src/Common/StdOutputStream.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Common.dir/Common/StdOutputStream.cpp.o"
+  "/root/coins/DirhamCli/src/Common/StreamTools.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Common.dir/Common/StreamTools.cpp.o"
+  "/root/coins/DirhamCli/src/Common/StringInputStream.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Common.dir/Common/StringInputStream.cpp.o"
+  "/root/coins/DirhamCli/src/Common/StringOutputStream.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Common.dir/Common/StringOutputStream.cpp.o"
+  "/root/coins/DirhamCli/src/Common/StringTools.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Common.dir/Common/StringTools.cpp.o"
+  "/root/coins/DirhamCli/src/Common/StringView.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Common.dir/Common/StringView.cpp.o"
+  "/root/coins/DirhamCli/src/Common/Util.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Common.dir/Common/Util.cpp.o"
+  "/root/coins/DirhamCli/src/Common/VectorOutputStream.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Common.dir/Common/VectorOutputStream.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "STATICLIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  "../../external"
+  "version"
+  "../../src/Platform/Linux"
+  "../../src/Platform/Posix"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
