0fbc39398752017f193c304d83e7bb1f4f65d538
LiteChain <38558930+LiteChain@users.noreply.github.com>
2018-04-23 15:19:51 +0200

    Add files via upload

---
+++ build/release/CMakeFiles/Transfers.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/coins/DirhamCli/src/Transfers/BlockchainSynchronizer.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Transfers.dir/Transfers/BlockchainSynchronizer.cpp.o"
+  "/root/coins/DirhamCli/src/Transfers/SynchronizationState.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Transfers.dir/Transfers/SynchronizationState.cpp.o"
+  "/root/coins/DirhamCli/src/Transfers/TransfersConsumer.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Transfers.dir/Transfers/TransfersConsumer.cpp.o"
+  "/root/coins/DirhamCli/src/Transfers/TransfersContainer.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Transfers.dir/Transfers/TransfersContainer.cpp.o"
+  "/root/coins/DirhamCli/src/Transfers/TransfersSubscription.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Transfers.dir/Transfers/TransfersSubscription.cpp.o"
+  "/root/coins/DirhamCli/src/Transfers/TransfersSynchronizer.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Transfers.dir/Transfers/TransfersSynchronizer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "STATICLIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  "../../external"
+  "version"
+  "../../src/Platform/Linux"
+  "../../src/Platform/Posix"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/release/CMakeFiles/Wallet.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/coins/DirhamCli/src/Wallet/LegacyKeysImporter.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Wallet.dir/Wallet/LegacyKeysImporter.cpp.o"
+  "/root/coins/DirhamCli/src/Wallet/WalletAsyncContextCounter.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Wallet.dir/Wallet/WalletAsyncContextCounter.cpp.o"
+  "/root/coins/DirhamCli/src/Wallet/WalletErrors.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Wallet.dir/Wallet/WalletErrors.cpp.o"
+  "/root/coins/DirhamCli/src/Wallet/WalletGreen.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Wallet.dir/Wallet/WalletGreen.cpp.o"
+  "/root/coins/DirhamCli/src/Wallet/WalletRpcServer.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Wallet.dir/Wallet/WalletRpcServer.cpp.o"
+  "/root/coins/DirhamCli/src/Wallet/WalletSerialization.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Wallet.dir/Wallet/WalletSerialization.cpp.o"
+  "/root/coins/DirhamCli/src/Wallet/WalletUtils.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Wallet.dir/Wallet/WalletUtils.cpp.o"
+  "/root/coins/DirhamCli/src/WalletLegacy/KeysStorage.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Wallet.dir/WalletLegacy/KeysStorage.cpp.o"
+  "/root/coins/DirhamCli/src/WalletLegacy/WalletHelper.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Wallet.dir/WalletLegacy/WalletHelper.cpp.o"
+  "/root/coins/DirhamCli/src/WalletLegacy/WalletLegacy.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Wallet.dir/WalletLegacy/WalletLegacy.cpp.o"
+  "/root/coins/DirhamCli/src/WalletLegacy/WalletLegacySerialization.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Wallet.dir/WalletLegacy/WalletLegacySerialization.cpp.o"
+  "/root/coins/DirhamCli/src/WalletLegacy/WalletLegacySerializer.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Wallet.dir/WalletLegacy/WalletLegacySerializer.cpp.o"
+  "/root/coins/DirhamCli/src/WalletLegacy/WalletTransactionSender.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Wallet.dir/WalletLegacy/WalletTransactionSender.cpp.o"
+  "/root/coins/DirhamCli/src/WalletLegacy/WalletUnconfirmedTransactions.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Wallet.dir/WalletLegacy/WalletUnconfirmedTransactions.cpp.o"
+  "/root/coins/DirhamCli/src/WalletLegacy/WalletUserTransactionsCache.cpp" "/root/coins/DirhamCli/build/release/src/CMakeFiles/Wallet.dir/WalletLegacy/WalletUserTransactionsCache.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "STATICLIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  "../../external"
+  "version"
+  "../../src/Platform/Linux"
+  "../../src/Platform/Posix"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
