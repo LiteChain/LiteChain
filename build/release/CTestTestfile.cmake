26046f52c6c3f6d0979fdd592dbd236e0d204002
LiteChain <38558930+LiteChain@users.noreply.github.com>
2018-04-23 15:12:17 +0200

    Add files via upload

---
+++ build/release/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /root/coins/DirhamCli
+# Build directory: /root/coins/DirhamCli/build/release
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("external")
+subdirs("src")
